243cb3f65ac90ce2edb74302177d7ecdb7bdb1e6
Gabriel <gabriel@gabriel-desktop.(none)>
2013-04-09 09:51:09 +0300

    Lists

--- learnpython.py
+++ learnpython.py
@@ -19,3 +19,28 @@ if isinstance(myfloat, float) and myfloat == 10.0:
 if isinstance (myint, int) and myint == 0:
 	print "Integer: %d" % myint
 
+
+#Lists
+'''In this exercise, you will need to add numbers and strings to the correct lists using the "append" 
+list method. You must add the numbers 1,2, and 3 to the "numbers" list, and the words 'hello' and 'world' 
+to the strings variable.You will also have to fill in the variable second_name with the second name in 
+the names list, using the brackets operator []. Note that the index is zero-based, so if you want to 
+access the second item in the list, its index will be 1.'''
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append("hello")
+strings.append("world")
+second_name = names [1]
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
